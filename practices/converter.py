8e05a366c3256bd9e0b6166201b2f86cf5b20811
Tuyen Tram <tctram1@catamount.wcu.edu>
2018-03-06 19:16:46 -0500

    Feet To Inches Def

---
+++ practices/converter.py
@@ -0,0 +1,38 @@
+def feetToInches():
+    feet = int(input("> Feet: "))
+    inches = int(input("> Inches: "))
+
+    totalInches = (feet * 12) + inches
+
+    print(str(feet) + "'" , str(inches) + '" is' , totalInches , "inches.")
+
+
+def inchesToFeet():
+    pass
+
+
+def meterToFeet():
+    pass
+
+
+def start():
+    print("""
+    Choose a converter:
+    1. ft+in - in
+    2. in - ft
+    3. m - ft+in
+    """)
+
+    choice = input("> ")
+
+    if choice == "1":
+        feetToInches()
+    elif choice == "2":
+        inchesToFeet()
+    elif choice == "3":
+        meterToFeet()
+    else:
+        print("Choose one of the above!")
+
+
+start()
